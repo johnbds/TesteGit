05590ab696dff2b24ec8dde793b07971dba78b57
Wanderley <ifnmg@192.168.163.19>
2019-03-19 20:08:53 -0300

    Inserção de dados

--- src/Concatenar.java
+++ src/Concatenar.java
@@ -11,5 +11,6 @@ public class Concatenar {
     public static void main(String[] args) {
         System.out.println("Teste" + " Ola");
         
+        System.out.println("Puuuuuuuuuuuullllllllllllll");
     }
 }
